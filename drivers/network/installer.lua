097920f820d9a35f5a23c224528da1213afaf61c
Mindels <44329587+MrMindels@users.noreply.github.com>
2026-02-21 06:39:13 +0100

    Update installer.lua
    fixed loop and added directory creation

--- drivers/network/installer.lua
+++ drivers/network/installer.lua
@@ -1,6 +1,14 @@
 files = {"config_manager.lua", "driver.lua", "init.lua"}
 base_url = "https://raw.githubusercontent.com/MrMindels/cc-tweaked-drivers/refs/heads/main/drivers/network/"
 
+if not fs.exists("programs/drivers/network") then
+    fs.makeDir("programs/drivers/network")   
+    print("Made needed directorie's")
+else
+    if not fs.isDir("programs/drivers/network") then
+        fs.makeDir("programs/drivers/network")
+    end
+end
 
 for _, file_name in ipairs(files) do
     local url = base_url .. file_name
